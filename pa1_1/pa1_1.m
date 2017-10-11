1433fa6c61595ed458ea73ba669cc1d5fc0315e9
aysun <aysunkocak@cs.hacettepe.edu.tr>
2017-10-11 15:13:25 +0300

    change starter code pa1_1

--- pa1_1/pa1_1.m
+++ pa1_1/pa1_1.m
@@ -1,13 +1,8 @@
 
 q = 255;
-I =  (imread('1.png'));
+I =  imread('1.png');
 
 I_q = round(I./q )*q ;
-imwrite(I_q,'quan.png')
 figure,imshow(I_q);
 
-D = Floyd_Steinberg_Dithering((I),q);
-imwrite(D,'fs.png')
-
-% I_q = round(D./q )*q ;
-figure,imshow(D);
+D = FloydSteinberg(I,q);
